fba42caf3c23d85d0468481e0062a744ee4ca260
Tina Chen <tinachen@MacBook-Pro.attlocal.net>
2021-04-20 23:03:11 -0700

    update users_admin and upload products list

---
+++ chen.tina/data/products.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 20, 2021 at 10:56 PM
+-- Server version: 5.6.49-cll-lve
+-- PHP Version: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tina_wnm608`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(13) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `category` varchar(64) NOT NULL,
+  `image_main` varchar(256) NOT NULL,
+  `price` decimal(9,2) NOT NULL,
+  `description` text NOT NULL,
+  `date_create` datetime NOT NULL,
+  `date_modify` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `category`, `image_main`, `price`, `description`, `date_create`, `date_modify`) VALUES
+(1, 'Heart of Pink', 'Roses', '/img/heart_pinkrose.jpeg', 110.00, 'Heart branded box style full of pink mix roses.', '2021-04-20 22:15:36', '2021-04-20 22:15:36'),
+(2, 'White Roses', 'Roses', '/img/whiterose.jpeg', 95.00, 'Elegant all white arrangement with creamy 16 white Roses and greens in a glass vase.', '2021-04-20 22:20:45', '2021-04-20 22:20:45'),
+(3, 'Sunrise Talk', 'Roses', '/img/yellowrose.jpeg', 69.00, 'Fill someone’s day with sunshine with our vibrant 24 yellow roses. These bright, cheerful blooms are a warm, thoughtful gift that will leave them beaming', '2021-04-20 22:22:38', '2021-04-20 22:22:38'),
+(4, 'Lavender Medley', 'Roses', '/img/lavender.jpeg', 59.00, 'Charming bouquet is loosely gathered with a medley of lavender & white blooms', '2021-04-20 22:24:28', '2021-04-20 22:24:28'),
+(5, 'All White', 'Roses', '/img/allwhite.jpeg', 77.00, 'White flowers have a timeless elegance, which is captured in our luxurious all-white arrangement', '2021-04-20 22:26:26', '2021-04-20 22:26:26'),
+(6, 'Spring Lilies', 'Lilies', '/img/spring_lilies.jpeg', 43.00, 'Send a sweet sentiment bouquet to someone special with our delightful spring lilies', '2021-04-20 22:31:24', '2021-04-20 22:31:24'),
+(7, 'Timeless Tulip', 'Tulips', '/img/timeless.jpeg', 128.00, '70 colorful tulips are an instant day-brightener. Whoever they’re for, whatever the reason, this cheerful bunch of spring blooms is the best kind of surprise.', '2021-04-20 22:33:20', '2021-04-20 22:33:20'),
+(8, 'Pure Red', 'Tulips', '/img/pure_red.jpg', 65.00, 'Elegant red tulips speak volumes! Beautifully hand-arranged with lush greenery inside our exclusive clear vase', '2021-04-20 22:40:38', '2021-04-20 22:40:38'),
+(9, 'Peruvian Lilies', 'Lilies', '/img/peruvian_lilies.jpeg', 79.00, '100 beautiful jewel-toned Peruvian lilies are a perfect surprise for someone precious to you.', '2021-04-20 22:40:38', '2021-04-20 22:40:38'),
+(10, 'Red Love', 'Roses', '/img/red_love.jpeg', 192.00, '100 red roses bring love to your side', '2021-04-20 22:51:11', '2021-04-20 22:51:11'),
+(11, 'Passionate Peaches', 'Mix', '/img/peaches.jpeg', 45.00, 'A soft and tender bouquet, perfect for expressing your gentle loving thoughts through delicate peach and light pink roses, carnations, alstroemeria', '2021-04-20 22:52:11', '2021-04-20 22:52:11'),
+(12, 'Wild Rainbow', 'Roses', '/img/rainbow.jpeg', 75.00, 'Wild 12 rainbow roses are an amazing gift that will blow them away on their special day.', '2021-04-20 22:55:16', '2021-04-20 22:55:16'),
+(13, 'The Firecracker', 'Mix', '/img/firecracker.jpeg', 70.00, 'Cool blue tones from thistle fit perfectly alongside bursts of orange roses and bright, golden Craspedia.', '2021-04-20 22:55:16', '2021-04-20 22:55:16');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- chen.tina/demo/users_admin.php
+++ chen.tina/demo/users_admin.php
@@ -2,13 +2,57 @@
 
 include "../lib/php/functions.php";
 
-$users = file_get_json("users.json");
+$filename = "users.json";
+$users = file_get_json($filename);
 
 // pretty_dump($_SERVER);
-// pretty_dump($_GET);
+// pretty_dump([$_GET,$_POST]);
 
 
 
+$empty_object = (object) [
+   "name"=>"",
+   "type"=>"",
+   "email"=>"",
+   "classes"=>[]
+];
+
+
+
+switch(@$_GET['crud']) {
+   case 'update':
+      $users[$_GET['id']]->name = $_POST['user-name'];
+      $users[$_GET['id']]->type = $_POST['user-type'];
+      $users[$_GET['id']]->email = $_POST['user-email'];
+      $users[$_GET['id']]->classes = explode(", ",$_POST['user-classes']);
+
+      file_put_contents($filename,json_encode($users));
+
+      header("location:{$_SERVER['PHP_SELF']}?id=".$_GET['id']);
+      break;
+   case 'create':
+      $empty_object->name = $_POST['user-name'];
+      $empty_object->type = $_POST['user-type'];
+      $empty_object->email = $_POST['user-email'];
+      $empty_object->classes = explode(", ",$_POST['user-classes']);
+
+      $id = count($users);
+
+      // array_push()
+      $users[] = $empty_object;
+
+      file_put_contents($filename,json_encode($users));
+
+      header("location:{$_SERVER['PHP_SELF']}?id=$id");
+      break;
+   case 'delete':
+      array_splice($users,$_GET['id'],1);
+
+      file_put_contents($filename,json_encode($users));
+
+      header("location:{$_SERVER['PHP_SELF']}");
+      break;
+}
 
 
 
@@ -16,86 +60,67 @@ $users = file_get_json("users.json");
 
 function showUserPage($user) {
 
+$id = $_GET['id'];
 $classes = implode(", ", $user->classes);
+$addoredit = $id=="new" ? 'Add' : 'Edit';
+$createorupdate = $id=="new" ? 'create' : 'update';
 
+
+// heredoc
 echo <<<HTML
-<nav class="nav crumbs">
-   <ul>
-      <li><a href="{$_SERVER['PHP_SELF']}">Back</a></li>
-   </ul>
+<div class="grid gap">
+<div class="col-xs-12">
+<div class="card soft">
+<nav class="nav pills display-flex">
+   <div class="flex-none"><a href="{$_SERVER['PHP_SELF']}"><img src="img/icon/arrow-left.svg" class="icon" style="font-size:1.5em"></a></div>
+   <div class="flex-stretch"></div>
+   <div class="flex-none"><a href="{$_SERVER['PHP_SELF']}?id=$id&crud=delete"><img src="img/icon/trash.svg" class="icon" style="font-size:1.5em"></a></div>
 </nav>
-<div>
-   <h2>$user->name</h2>
-   <div>
-      <strong>Type</strong>
-      <span>$user->type</span>
-   </div>
-   <div>
-      <strong>Email</strong>
-      <span>$user->email</span>
-   </div>
-   <div>
-      <strong>Classes</strong>
-      <span>$classes</span>
+</div>
+</div>
+<div class="col-xs-12 col-md-4">
+   <div class="card soft">
+      <h2>$user->name</h2>
+      <div>
+         <strong>Type</strong>
+         <span>$user->type</span>
+      </div>
+      <div>
+         <strong>Email</strong>
+         <span>$user->email</span>
+      </div>
+      <div>
+         <strong>Classes</strong>
+         <span>$classes</span>
+      </div>
    </div>
 </div>
-
-
-         <div class="grid gap" style="margin-top: 3em">
-          <div class="col-xs-12 col-md-6">
-            <div class="form-control">
-            <label for="example1" class="form-label">First Name</label>
-            <input id="example1" type="text" placeholder="$user->firstname" class="form-input">
-            </div>
-          </div>
-
-         <div class="col-xs-12 col-md-6">
-          <div class="form-control">
-            <label for="example2" class="form-label">Last Name</label>
-            <input id="example2" type="text" placeholder="$user->lastname" class="form-input">
-          </div>
-         </div>
-       </div>
-
-
-         <div class="grid gap">
-         <div class="col-xs-12 col-md-6">
-          <div class="form-control">
-            <label for="example3" class="form-label">Type</label>
-            <input id="example3" type="text" placeholder="$user->type" class="form-input">
-          </div>
-         </div>
-
-         <div class="col-xs-12 col-md-6">
-          <div class="form-control">
-            <label for="example3" class="form-label">Email</label>
-            <input id="example3" type="text" placeholder="$user->email" class="form-input">
-          </div>
-         </div>
-       </div>
-
-
-         <div class="grid gap">
-         <div class="col-xs-12 col-md-4">
-          <div class="form-control">
-            <label for="example3" class="form-label">Classes</label>
-            <input id="example3" type="text" placeholder="$classes" class="form-input">
-            </div>
-         </div>
-         </div>
-          
-         <div>
-         <div class="display-flex">
-            <div class="flex-stretch">
-         </div>
-            <div class="flex-none" style="margin-top: 2em ">
-            <a href="index.php">
-               <button type="button" class="form-button sell">Submit</button>
-            </a>
-            </div>
-         </div>
-
-
+<form class="col-xs-12 col-md-8" method="post" action="{$_SERVER['PHP_SELF']}?id=$id&crud=$createorupdate">
+   <div class="card soft">
+      <h2>$addoredit User</h2>
+      <input type="hidden" name="id" value="$id">
+      <div class="form-control">
+         <label class="form-label" for="user-name">Name</label>
+         <input class="form-input"type="text" id="user-name" name="user-name" value="$user->name">
+      </div>
+      <div class="form-control">
+         <label class="form-label" for="user-type">Type</label>
+         <input class="form-input"type="text" id="user-type" name="user-type" value="$user->type">
+      </div>
+      <div class="form-control">
+         <label class="form-label" for="user-email">Email</label>
+         <input class="form-input"type="email" id="user-email" name="user-email" value="$user->email">
+      </div>
+      <div class="form-control">
+         <label class="form-label" for="user-classes">Classes</label>
+         <input class="form-input"type="text" id="user-classes" name="user-classes" value="$classes">
+      </div>
+      <div class="form-control">
+         <input class="form-button" style="margin-top: 2em;" type="submit" value="Submit">
+      </div>
+   </div>
+</form>
+</div>
 HTML;
 }
 
@@ -119,20 +144,26 @@ HTML;
          <nav class="flex-none nav flex">
             <ul>
                <li><a href="<?= $_SERVER['PHP_SELF'] ?>">List</a></li>
+               <li><a href="<?= $_SERVER['PHP_SELF'] ?>?id=new">Add New User</a></li>
             </ul>
          </nav>
       </div>
    </header>
 
    <div class="container">
-      <div class="card soft">
 
          <?php
          if(isset($_GET['id'])) {
-            showUserPage($users[$_GET['id']]);
+            // ternary, conditional
+            showUserPage(
+               $_GET['id']=="new" ?
+               $empty_object :
+               $users[$_GET['id']]
+            );
          } else {
          ?>
 
+      <div class="card soft">
          <h2>User List</h2>
 
          <ul>
@@ -146,10 +177,10 @@ HTML;
 
          ?>
          </ul>
+      </div>
          <?php
          }
          ?>
-      </div>
    </div>
 </body>
 </html>
